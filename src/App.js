b3ce4c93693e04a8a86476552d16bff89ab48c50
maxime <maximelarquetoux@gmail.com>
2025-06-15 00:11:41 +0200

    Implementing Random Joke Retrieval

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [joke, setJoke]= useState(null);
+  const [error, setError]= useState(null);
+
+  const fetchRandomJoke= async ()=>{
+    setJoke(null);
+    setError(null);
+    try {
+      const response= await axios.get("http://localhost:4000/api/jokes/random");
+      setJoke(response.data);
+    } catch (error) {
+      console.error("Error fetching random joke: ", error);
+      if(error.response){
+        setError(error.response.data.message || `Server Error: ${error.response.status}`);
+      }else if(error.request){
+        setError("No server response. Check the connection or state of backend");
+      }else{
+        setError(error.message);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Carambar & Co</h1>
+        <button onClick={fetchRandomJoke}>Afficher une Blague Aléatoire</button>
+
+        {error && <p>Erreur: {error}</p>}
+
+        {joke && (
+          <div>
+            <p> {joke.question} </p>
+            <p> {joke.response} </p>
+            {joke.id && <small> (Cliquer pour re-afficher cette blague: {joke.id}) </small>}
+          </div>
+        )}
+        {!joke && !error && <p>Cliquez sur le bouton pour découvrir une blague !</p>}
       </header>
     </div>
   );
